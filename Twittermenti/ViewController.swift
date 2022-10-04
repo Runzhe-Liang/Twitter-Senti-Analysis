549acd718ac0f35010330995eb78c7b707a24fef
Irvingrie <runzhe_liang@163.com>
2022-10-04 12:56:30 -0400

    set up Twitter developer API

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,16 +7,25 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let swifter = Swifter(consumerKey: "UrYwdEoYSm33RbPXEYeI5p0Ph", consumerSecret: "dzVl9dcm3sSwjXDa6Z9881d0eglqz4jeG4fp502dumnYItLSLT")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        swifter.searchTweet(using: "@China") { result, searchMetadata in
+            print(result)
+        } failure: { error in
+            print("There was an error with the Twitter API request, \(error)")
+        }
+
     }
 
     @IBAction func predictPressed(_ sender: Any) {
